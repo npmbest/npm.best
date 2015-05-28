4c240d7df7ea63516c376b2c717352b594330b60
Zongmin Lei <leizongmin@gmail.com>
2015-05-28 12:43:30 +0800

    修正搜索关键字过滤正则

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -100,7 +100,10 @@ $(document).ready(function () {
   
   // 显示模块列表
   function showPackagesPage (query, skip, limit) {
-    query = query.trim().replace(/[^0-9a-zA-Z.\-_$+ ]/g, '').replace(/\s/, '+');
+    query = query.trim();
+    //.replace(/[^0-9a-zA-Z.\-_$+ ]/g, '');
+    query = query.replace(/[^0-9a-zA-Z.\-_$ +\u3400-\u4DB5\u4E00-\u9FA5\u9FA6-\u9FBB\uF900-\uFA2D\uFA30-\uFA6A\uFA70-\uFAD9\uFF00-\uFFEF\u2E80-\u2EFF\u3000-\u303F\u31C0-\u31EF\u2F00-\u2FDF\u2FF0-\u2FFF\u3100-\u312F\u31A0-\u31BF\u3040-\u309F\u30A0-\u30FF\u31F0-\u31FF\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F\u4DC0-\u4DFF\uA000-\uA48F\uA490-\uA4CF\u2800-\u28FF\u3200-\u32FF\u3300-\u33FF\u2700-\u27BF\u2600-\u26FF\uFE10-\uFE1F\uFE30-\uFE4F]/g, '')
+    query = query.replace(/\s/, '+');
     $('#ipt-search').val(query);
     queryPackages(query, skip, limit, function (err, ret) {
       if (err) return messageBox.error(err);
