000b06db37790e35fc28907aad85586e60c4e4f5
Zongmin Lei <leizongmin@gmail.com>
2015-05-31 10:06:59 +0800

    服务器端页面i18n

--- web/index.js
+++ web/index.js
@@ -13,12 +13,23 @@ var express = require('express');
 var serveStatic = require('serve-static');;
 var cookieParser = require('cookie-parser');
 var expressLiquid = require('express-liquid');
+var i18n = require("i18n");
+
+
+i18n.configure({
+  locales:['en', 'zh'],
+  defaultLocale: 'en',
+  cookie: 'language',
+  directory: path.resolve(__dirname, './i18n'),
+  updateFiles: false
+});
 
 
 var app = express();
 app.use('/assets', serveStatic(path.resolve(__dirname, 'assets')));
 app.use(cookieParser(config.get('web.cookie.secret')));
 app.use(middleware.apiUtils);
+app.use(i18n.init);
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', expressLiquid({
@@ -26,6 +37,10 @@ app.engine('html', expressLiquid({
   traceError: true
 }));
 app.use(expressLiquid.middleware);
+app.use(function (req, res, next) {
+  res.context.setFilter('i18n', res.__);
+  next();
+});
 
 
 app.use('/api', middleware.cache);
