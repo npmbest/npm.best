0cb1e6b428212784375e3ec7e70ba8b9e33f3cc0
Zongmin Lei <leizongmin@gmail.com>
2015-11-18 13:14:47 +0800

    tool/download_npm_registry 支持显示下载进度

--- tool/download_npm_registry.js
+++ tool/download_npm_registry.js
@@ -1,25 +1,31 @@
+'use strict';
+
 /**
  * npm.best
- * 
+ *
  * @author Zongmin Lei <leizongmin@gmail.com>
  */
 
-var path = require('path');
-var config = require('../config');
-var utils = require('../lib/utils');
-var mkdirp = require('mkdirp');
+let path = require('path');
+let config = require('../config');
+let utils = require('../lib/utils');
+let mkdirp = require('mkdirp');
 
 
-var file = path.resolve(config.get('path.data'), config.get('define.npm.registryFileName'));
-var url = config.get('define.npm.registry') + '/-/all';
+let file = path.resolve(config.get('path.data'), config.get('define.npm.registryFileName'));
+let url = config.get('define.npm.registry') + '/-/all';
 console.log('download from %s', url);
 
 mkdirp.sync(path.dirname(file));
 
-var timestamp = Date.now();
-utils.download(url, file, function (err, file) {
+let timestamp = Date.now();
+utils.download(url, file, (size, total) => {
+  process.stdout.write(utils.clc.move.to(0, utils.clc.windowSize.height));
+  //process.stdout.write(utils.clc.erase.line);
+  process.stdout.write(utils.format('process: %s%% [%s/%s]', (size / total * 100).toFixed(1), size, total));
+}, (err, file) => {
   if (err) throw err;
-  var spent = Date.now() - timestamp;
+  let spent = Date.now() - timestamp;
   console.log('save to %s', file);
   console.log('spent %sms', spent);
   process.exit();
