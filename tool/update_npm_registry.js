68de643067673901d73f76b5ad8b53fa1ace7efa
Zongmin Lei <leizongmin@gmail.com>
2015-11-18 15:07:54 +0800

    update tool update_pm_registry

--- tool/update_npm_registry.js
+++ tool/update_npm_registry.js
@@ -1,22 +1,24 @@
+'use strict';
+
 /**
  * npm.best
- * 
+ *
  * @author Zongmin Lei <leizongmin@gmail.com>
  */
 
-var path = require('path');
-var fs = require('fs');
-var config = require('../config');
-var utils = require('../lib/utils');
-var model = require('../lib/model');
-var async = require('async');
+let path = require('path');
+let fs = require('fs');
+let config = require('../config');
+let utils = require('../lib/utils');
+let model = require('../lib/model');
+let async = require('async');
 
 
-var file = path.resolve(config.get('path.data'), config.get('define.npm.registryFileName'));
+let file = path.resolve(config.get('path.data'), config.get('define.npm.registryFileName'));
 console.log('load package list...');
 
 function getLatestVersion (versions) {
-  for (var i in versions) {
+  for (let i in versions) {
     if (versions[i] === 'latest') {
       return i;
     }
@@ -28,27 +30,27 @@ function isGithubRepository (url) {
   return ((url || '').indexOf('github.com') !== -1 ? 1 : 0);
 }
 
-var timestamp = Date.now();
-var counter = 0;
+let timestamp = Date.now();
+let counter = 0;
 
-var JSONStream = require('JSONStream');
-var jsonStream = JSONStream.parse('*');
-var fileStream = fs.createReadStream(file);
+let JSONStream = require('JSONStream');
+let jsonStream = JSONStream.parse('*');
+let fileStream = fs.createReadStream(file);
 fileStream.pipe(jsonStream);
-var isEnd = false;
-var pending = 0;
-var list = [];
-jsonStream.on('data', function (data) {
+let isEnd = false;
+let pending = 0;
+let list = [];
+jsonStream.on('data', (data) => {
   fileStream.pause();
   list.push(data);
   updateNextItem();
 });
-jsonStream.on('end', function () {
+jsonStream.on('end', () => {
   isEnd = true;
   updateNextItem();
 });
 
-var isPending = false;
+let isPending = false;
 function updateNextItem () {
   if (isPending) return;
   if (list.length < 1) {
@@ -59,8 +61,8 @@ function updateNextItem () {
     }
   } else {
     isPending = true;
-    var item = list.shift();
-    onItem(item, function () {
+    let item = list.shift();
+    onItem(item, () => {
       isPending = false;
       process.nextTick(updateNextItem);
     });
@@ -81,81 +83,49 @@ function formatLicense (license) {
 }
 
 
-function onItem (item, next) {
-  
+function onItem (item, callback) {
+  if (!item.name) return callback();
+
   counter++;
   pending++;
   console.log('[%s] update package %s', counter, item.name);
-  async.series([
-    function (next) {
-      
-      model.npm_packages.findOne({name: item.name}, function (err, ret) {
-        if (err) return next(err);
-        if (ret) {
-          model.npm_packages.update({name: item.name}, {
-            json_data: JSON.stringify(item),
-            updated: new Date()
-          }, next);
-        } else {
-          model.npm_packages.create({
-            name: item.name,
-            json_data: JSON.stringify(item),
-            updated: new Date()
-          }, next);
-        }
-      });
-      
-    },
-    function (next) {
-      
-      model.packages.findOne({name: item.name}, function (err, ret) {
-        if (err) return next(err);
-        var data = {
-          latest_version: getLatestVersion(item.versions),
-          modified: (item.time && item.time.modified) || '',
-          license: formatLicense((item.license && item.license.type) || item.license || ''),
-          repository: (item.repository && item.repository.url) || '',
-          is_github: isGithubRepository(item.repository && item.repository.url),
-          homepage: item.homepage || '',
-          bugs: (item.bugs && item.bugs.url) || '',
-          keywords: (item.keywords || []).toString(),
-          description: item.description || '',
-          author_name: (item.author && item.author.name) || '',
-          author_email: (item.author && item.author.email) || ''
-        };
-        for (var i in data) {
-          data[i] = String(data[i]);
-        }
-        if (ret) {
-          model.packages.update({name: item.name}, data, next);
-        } else {
-          data.name = item.name;
-          model.packages.create(data, next);
-        }
-      });
-      
-    },
-    function (next) {
-      
-      model.package_keywords.deleteAll({package: item.name}, function (err) {
-        if (err) return next(err);
-        if (!(Array.isArray(item.keywords) && item.keywords.length > 0)) return next();
-        var list = item.keywords.map(function (keyword) {
-          return {package: item.name, keyword: keyword};
-        });
-        model.package_keywords.create(list, next);
-      });
-      
+
+  model.packages.findOne({name: item.name}, (err, ret) => {
+    if (err) return callback(err);
+    let data = {
+      latest_version: getLatestVersion(item.versions),
+      modified: (item.time && item.time.modified) || '',
+      license: formatLicense((item.license && item.license.type) || item.license || ''),
+      repository: (item.repository && item.repository.url) || '',
+      is_github: isGithubRepository(item.repository && item.repository.url),
+      homepage: item.homepage || '',
+      bugs: (item.bugs && item.bugs.url) || '',
+      keywords: (item.keywords || []).toString(),
+      description: item.description || '',
+      author_name: (item.author && item.author.name) || '',
+      author_email: (item.author && item.author.email) || ''
+    };
+
+    let cb = (err) => {
+      if (err) {
+        console.log('  - %s', err);
+      } else {
+        console.log('  - ok');
+      }
+      callback();
+    };
+
+    for (let i in data) {
+      data[i] = String(data[i]);
     }
-  ], function (err) {
-    if (err) {
-      console.log('  - %s', err);
+    if (ret) {
+      model.packages.update({name: item.name}, data, cb);
     } else {
-      console.log('  - ok');
+      data.name = item.name;
+      model.packages.create(data, cb);
     }
-    next();
   });
-  
+
 }
 
 function onEnd (err) {
